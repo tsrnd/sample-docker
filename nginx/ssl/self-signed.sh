13253fab68efc38f383704164874d82b148f2329
Dao Nguyen V <daonv@asiantech.vn>
2016-11-17 17:55:24 +0700

    test trusted user

--- nginx/ssl/self-signed.sh
+++ nginx/ssl/self-signed.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-openssl genrsa -des3 -out server.key 2048
-openssl req -new -key server.key -out server.csr
-cp server.key server.key.org
-openssl rsa -in server.key.org -out server.key
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+# Generate server certificate
+openssl genrsa -out server.key 4096
+openssl req -new -x509 -days 365 -key server.key -out server.crt
+
+# Generate client certificate
+openssl genrsa -out client.key 4096
+openssl req -new -key client.key -out client.csr
+
+# self-signed
+openssl x509 -req -days 365 -in client.csr -CA server.crt -CAkey server.key -set_serial 01 -out client.crt
+
+# Convert client key to PKCS (for browsers)
+openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.p12
+# Make sure you enter an export password. Otherwise, you may be unable to import it to your browser.

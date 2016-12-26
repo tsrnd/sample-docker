f3c86e435e084cc67f906555b367a8bf02612352
Dao Nguyen V <daonv@asiantech.vn>
2016-12-26 16:24:25 +0700

    update proxy config, all done!!!

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,15 +10,10 @@ log() {
     echo "> $1"
 }
 
-SECTION 'Clean'
-
-docker-machine ls -q | grep -v default | xargs docker-machine rm -y
-
 SECTION 'Define'
 
 iso_url="file://$HOME/.docker/machine/boot2docker.iso"
 registry='172.16.110.141:5000'
-repo='asiantech/ts-server'
 machine_flags="-d virtualbox
     --virtualbox-boot2docker-url $iso_url
     --virtualbox-memory 1024
@@ -35,15 +30,21 @@ net_public='swarm-public'
 
 app_name='chat'
 app_dir='./app/chat'
-app_image="$registry/$repo/$app_name:0.0.0"
+app_ver='0.0.0'
+app_image="$registry/$app_name:$app_ver"
 app_scale='2'
+app_port='3000'
 
 db_name='redis'
-db_dir='./data/redis'
-db_image="$registry/$repo/$db_name:0.0.0"
+db_image="$registry/redis:3.2.5"
+db_port='6379'
 
 proxy_name='proxy'
 proxy_image="$registry/dockercloud/haproxy:1.6.2"
+proxy_port='80'
+proxy_stats_port='1936'
+proxy_stats_user='admin'
+proxy_stats_pass='admin'
 
 create_node() {
     local name="$1"
@@ -64,14 +65,13 @@ SECTION 'Images'
 create_node default
 eval $(docker-machine env default)
 log "build $app_image"
+docker images | grep "$registry/$app_name" | grep "$app_ver" && docker rmi $app_image > /dev/null
 docker build $app_dir -t $app_image > /dev/null
 docker push $app_image > /dev/null
-log "build $db_image"
-docker build $db_dir -t $db_image > /dev/null
-docker push $db_image > /dev/null
 
 SECTION 'Swarm Nodes'
 
+docker-machine ls -q | grep -v default | xargs docker-machine rm -y > /dev/null
 for node in "${swarm_nodes[@]}"; do
     create_node "$node"
 done
@@ -81,6 +81,8 @@ token=''
 swarm_ip="$(docker-machine ip $swarm_master)"
 for node in "${swarm_nodes[@]}"; do
     eval $(docker-machine env $node)
+    log 'remove old containers'
+    # docker ps -aq | xargs docker rm -f
     [[ $node == $swarm_master ]] && {
         log "init swarm at node '$node'"
         docker node ls || {
@@ -117,40 +119,40 @@ docker network ls | grep "$net_public" \
 SECTION 'Services'
 
 log "create '$proxy_name'"
-docker service ls | grep "$proxy_name" \
-    || docker service create \
-        --name $proxy_name \
-        --mode global \
-        --network $net_local \
-        --network $net_public \
-        --mount target=/var/run/docker.sock,source=/var/run/docker.sock,type=bind \
-        --constraint 'node.role == manager' \
-        -p 80:80 \
-        $proxy_image
+docker service ls | grep "$proxy_name" && docker service rm $proxy_name > /dev/null
+docker service create \
+    --name $proxy_name \
+    --mode global \
+    --network $net_local \
+    --network $net_public \
+    --mount target=/var/run/docker.sock,source=/var/run/docker.sock,type=bind \
+    --constraint "node.role == manager" \
+    -p "$proxy_port:$proxy_port" \
+    -p "$proxy_stats_port:$proxy_stats_port" \
+    -e STATS_AUTH="$proxy_stats_user:$proxy_stats_pass" \
+    $proxy_image
 
 log "create '$db_name'"
-docker service ls | grep "$db_name" \
-    || docker service create \
-        --name $db_name \
-        --replicas 1 \
-        --network $net_local \
-        $db_image
+docker service ls | grep "$db_name" && docker service rm $db_name > /dev/null
+docker service create \
+    --name $db_name \
+    --replicas 1 \
+    --network $net_local \
+    --constraint "node.role != manager" \
+    -p "$db_port:$db_port" \
+    $db_image
 
 log "create '$app_name'"
-docker service ls | grep "$app_name" \
-    || docker service create \
-        --name $app_name \
-        --replicas 1 \
-        --network $net_local \
-        --constraint 'node.role != manager' \
-        -e DB=$db_name \
-        -e SERVICE_PORTS=80 \
-        $app_image
-
-SECTION 'Update'
-
-docker service scale $app_name=2
-docker service update $app_name
+docker service ls | grep "$app_name" && docker service rm $app_name > /dev/null
+docker service create \
+    --name $app_name \
+    --replicas 1 \
+    --network $net_local \
+    --constraint "node.role != manager" \
+    -e SERVICE_PORTS="$app_port" \
+    -e DB="$db_name" \
+    -p "$app_port:$app_port" \
+    $app_image
 
 SECTION 'Info'
 

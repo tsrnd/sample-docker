135ba5c379ed57b2101aaa8074d44ae91cb582ee
Dao Nguyen V <dao.nguyen@asiantech.vn>
2017-02-07 11:57:23 +0700

    config ide

---
+++ web/app/Models/Task.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Task
+ *
+ * @mixin \Eloquent
+ */
+class Task extends Model
+{
+    //
+}
--- web/routes/web.php
+++ web/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
+use App\Task;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +17,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', function (Request $request) {
+//    $validator =
+    return view('tasks');
+})->middleware('auth:node');
+
+Route::get('/tasks/{task}', function (Request $request) {
+    return view('task');
+})->middleware('auth:node');
+
+Route::post('/tasks/{task}', function (Request $request) {
+    //
+})->middleware('auth:node');
+
+Route::delete('/tasks/{task}', function (Request $request) {
+    //
+})->middleware('auth:node');
